aa3d37663e84bbe9d6e32a5bd1f8bd7552bc304d
Balakrishnan, Vishnupriya <v.z.balakrishnan@accenture.com>
2025-08-26 00:33:03 +0200

    Initial project setup

---
+++ src/main/java/com/assignment/ClientManagementApplication.java
@@ -0,0 +1,12 @@
+package com.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientManagementApplication.class, args);
+    }
+}
