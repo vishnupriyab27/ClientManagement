f20b334eea5419139cd6b78667e751df774aba06
Balakrishnan, Vishnupriya <v.z.balakrishnan@accenture.com>
2025-08-26 01:52:12 +0200

    Changed ClientRepository as an interface extending from JPARepository

--- src/main/java/com/assignment/repository/ClientRepository.java
+++ src/main/java/com/assignment/repository/ClientRepository.java
@@ -1,4 +1,12 @@
 package com.assignment.repository;
 
-public class ClientRepository {
+import com.assignment.entity.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ClientRepository extends JpaRepository<Client, UUID> {
+    boolean existsByEmail(String email);
+    Optional<Client> findByEmail(String email);
 }
